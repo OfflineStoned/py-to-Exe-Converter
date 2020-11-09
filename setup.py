72850acaa18f5348684c9786e5c2d0b27d16c0cd
Disrespect = Disconnect| <74122632+OfflineStoned@users.noreply.github.com>
2020-11-09 18:24:33 +1000

    Please put Credits if your Going to share this
    U need Python Version 2.7 Installed with idle.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import cx_Freeze
+from cx_Freeze import * 
+
+setup (
+    name = "Botnet__Scrapper",
+
+    executables = [
+        Executable(
+            "Discord bot.py", 
+
+        )
+
+    ]
+
+)
